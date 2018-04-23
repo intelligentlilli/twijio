700f8405d1398ead33c20f686c547fe7c646f9c3
Russ Anderson <russ@kemurisense.com>
2018-04-23 10:57:33 +0100

    Update http-kit

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
                  [cheshire "5.8.0"]
-                 [http-kit "2.2.0"]])
+                 [http-kit "2.3.0"]])
