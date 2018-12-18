b72f9b01ed4014d4b222e6165c9b703896a0deac
Russ Anderson <russ@kemurisense.com>
2018-12-18 10:42:21 +0000

    Upgrade clojure version to most recent

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/kemurisense/twijio/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]
                  [cheshire "5.8.1"]
                  [http-kit "2.3.0"]])
