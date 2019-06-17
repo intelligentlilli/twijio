021497d72c7fe613872d1e98a11652a1a9d937fe
Russ Anderson <russ@kemurisense.com>
2019-06-17 10:54:03 +0100

    Bump of dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject kemurisense/twijio "0.1.3"
+(defproject kemurisense/twijio "0.1.4"
   :description "Pure Clojure library for interacting with Twilio's REST API"
   :url "https://github.com/kemurisense/twijio/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.async "0.4.490"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "0.4.500"]
                  [cheshire "5.8.1"]
                  [http-kit "2.3.0"]])
