f23c451875e7a742fa8497d6e25d4d3c2280b271
Russ Anderson <russ.anderson@gmail.com>
2020-10-09 10:00:32 +0100

    Bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kemurisense/twijio "0.1.11"
+(defproject kemurisense/twijio "0.1.12"
   :description "Pure Clojure library for interacting with the Twilio REST API, to send SMS messages"
   :url "https://github.com/kemurisense/twijio/"
   :license {:name "Eclipse Public License"
