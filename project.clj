ca972e5958ea2baaa462c880420f59abd2789c8d
Russ Anderson <russ.anderson@gmail.com>
2022-03-21 13:38:28 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject kemurisense/twijio "0.1.12"
+(defproject kemurisense/twijio "0.1.13"
   :description "Pure Clojure library for interacting with the Twilio REST API, to send SMS messages"
   :url "https://github.com/kemurisense/twijio/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.3.610"]
-                 [cheshire "5.10.0"]
-                 [http-kit "2.5.0"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.5.648"]
+                 [cheshire "5.10.2"]
+                 [http-kit "2.5.3"]])
